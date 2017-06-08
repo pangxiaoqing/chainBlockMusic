d4a662f37fc1083573df9b10a830a8add16d951b
pangxiaoqing <xiaoqing246810@163.com>
2017-06-08 14:27:53 +0800

    add

--- public/js/controller/generateStringByTemplate.js
+++ public/js/controller/generateStringByTemplate.js
@@ -18,4 +18,34 @@ function generateStringByTemplate(obj){
   return new_arr;
 }
 
+//时间戳处理函数
+  function changeByReg(num){
+    num = num || 1493103810;
+    var newDate = new Date();
+    newDate.setTime(num * 1000);
+    var str = newDate.toLocaleString()
+    var reg_a = /[\u4e00-\u9fa5]|[/]/g
 
+    var newstr = str.replace(reg_a,function(t){
+      if(t === '/'){
+        return '-';
+      }
+      if(/[\u4e00-\u9fa5]/.test(t)){
+        return '';
+      }
+    })
+    return newstr;
+  }
+
+
+  function ajax(url,type,timeout,data,callback){
+    $.ajax({
+        url:url,
+        type:type,
+        data:data,
+        timeout : timeout,
+        success: function(data){
+          callback(data);
+        }
+      });
+  }
--- public/js/controller/song.js
+++ public/js/controller/song.js
@@ -48,42 +48,28 @@ function aa(){
 	}, false);
 
 	var $playpause = $("#playpause");
-	var $stepForward = $("#stepForward");
-	var $stepBackward = $("#stepBackward");
+	var $val = $playpause.attr("data-val");
+	console.log($val);
+	// var $stepForward = $("#stepForward");
+	// var $stepBackward = $("#stepBackward");
 	//页面加载完成 直接播放音乐
-	$playpause.removeClass("glyphicon-play").addClass("glyphicon-pause");
-	audio.play();
-	updateProgress();
+	// $playpause.removeClass("glyphicon-play").addClass("glyphicon-pause");
+	// audio.play();
+	// updateProgress();
 	//点击切换暂停播放按键
-	$playpause.click(function(){
-		if(audio.paused || audio.ended) {
-				$playpause.removeClass("glyphicon-play").addClass("glyphicon-pause");
-		    audio.play();
-		    updateProgress();
-		}else{
-		    $playpause.removeClass("glyphicon-pause").addClass("glyphicon-play");
-		    audio.pause();
-		}
-	});
-	//点击下一首歌，歌曲切换至下一首
-	// $stepForward.click(function(){
-	// 	// console.log($.cookie("num"));
-	// 	$num += 1;
-	// 	if($num > 16){
-	// 		alert("当前歌曲是最后一首歌");
-	// 	}else{
-	// 		resetGetJsonData($num);
-	// 	}
-	// });
-	//点击快进按钮，向后退
-	// $stepBackward.click(function(){
-	// 	$num -= 1;
-	// 	if($num < 1){
-	// 		alert("当前歌曲是第一首歌");
-	// 	}else{
-	// 		resetGetJsonData($num);
-	// 	}
-	// });
+	if($val === 'play'){
+		console.log(11);
+		$playpause.click(function(){
+			if(audio.paused || audio.ended) {
+					$playpause.removeClass("glyphicon-play").addClass("glyphicon-pause");
+			    audio.play();
+			    updateProgress();
+			}else{
+			    $playpause.removeClass("glyphicon-pause").addClass("glyphicon-play");
+			    audio.pause();
+			}
+		});
+	}
 	//定时器函数，控制进度条进度
 	function updateProgress(){
 		var $progressWidth = $("#progress").width();
@@ -97,37 +83,10 @@ function aa(){
     //console.log(ProcessYet);
     $("#progressBar").css("width",ProcessYet);
 	}
-
-
   //监听媒体文件结束的事件（ended），这边一首歌曲结束循环播放
 	audio.addEventListener('ended', function() {
     audio.currentTime = 0;
     audio.play();
     updateProgress();
 	});
-	// //监听媒体文件结束的事件（ended），这边一首歌曲结束就读取下一首歌曲，实现播放上的循环播放
-	// audio.addEventListener('ended', function() {
-	// 	if($num == 16){
-	// 		$num = 1;
-	// 		resetGetJsonData($num);
-	// 	}else{
-	// 		$num += 1;
-	// 		resetGetJsonData($num);
-	// 	}
-	// });
-	//当cookie中存的num改变时，重新从json文件中获取数据，加载到界面上
-	// function resetGetJsonData(num){
-	// 	$.getJSON('js/song.json', function(data){
-	// 		// console.log(data);
-	// 		$.cookie("num",num);
-	// 		// console.log(data[$num]);
-	// 		$("#songMess").empty().append(generateStringByTemplate({
-	// 			id:"songMessage",
-	// 			params:{"%d":"name","%t":"describe","%s":"src"},
-	// 			data:data[num]
-	// 		}));
-	// 		//刷新界面相当与F5的功能（执行完次动作后，相当与return，所以在此之后的事件将不会执行）
-	// 		window.location.reload();
-	// 	});
-	// }
 };
